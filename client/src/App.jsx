aabad58a9b0623b23fe5d7b1f1c671580c4a1db9
SATYAM KUMAR PANDEY <defencepur360@gmail.com>
2024-07-10 13:54:03 +0530

    install react js and tailwind css and create the first template

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+
+
+function App() {
+  return (
+    <>
+      <div className=" w-full px-20 py-5 mx-auto text-center min-h-screen">
+        <h1
+          className="text-3xl font-bold
+         bg-slate-400 p-6 rounded-md"
+        >
+          This is my 2nd full stack project using MERN stack: Blog app{" "}
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
