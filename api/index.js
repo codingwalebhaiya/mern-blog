6ddf60590e1bb4e61592b193fa14d328b277d619
SATYAM KUMAR PANDEY <defencepur360@gmail.com>
2024-07-12 15:47:39 +0530

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,25 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv"; 
+
+dotenv.config();
+ 
+
+mongoose 
+  .connect(process.env.MONGODB_URI, { 
+    useNewUrlParser: true,
+    useUnifiedTopology: true, 
+  })
+  .then(() => {
+    console.log("Connected to MongoDB Successfully!!");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB", err);
+  });
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-})
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
