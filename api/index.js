97318c444bc0ebfe7abc119b0c98e80b1d1358c7
SATYAM KUMAR PANDEY <defencepur360@gmail.com>
2024-07-13 15:39:54 +0530

    create signup api route

--- api/index.js
+++ api/index.js
@@ -1,15 +1,16 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv"; 
-import userRouter from "./src/routes/user.route.js"
+import userRouter from "./src/routes/user.route.js";
+import authRouter from "./src/routes/auth.route.js";
 
 dotenv.config();
  
 
 mongoose 
   .connect(process.env.MONGODB_URI, { 
-    useNewUrlParser: true,
-    useUnifiedTopology: true, 
+       useNewUrlParser: true,
+       useUnifiedTopology: true, 
   })
   .then(() => {
     console.log("Connected to MongoDB Successfully!!");
@@ -19,10 +20,19 @@ mongoose
   });
 
 const app = express();
+
+app.use(express.json());
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
+
+// create routes - 
+
 app.use('/api/user', userRouter);
+
+app.use('/api/auth', authRouter);
+
