be019cd754e5c336ec1421f164fec9e424ccaa54
SATYAM KUMAR PANDEY <defencepur360@gmail.com>
2024-07-15 15:11:03 +0530

    create signin api route

--- api/src/controllers/auth.controller.js
+++ api/src/controllers/auth.controller.js
@@ -1,8 +1,9 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/errorHandler.js";
+import jwt from "jsonwebtoken";
 
-
+// user sign up - 
 const signUp = async (req, res, next) => {
   const { username, email, password } = req.body;
 
@@ -14,11 +15,11 @@ const signUp = async (req, res, next) => {
     email === "" ||
     password === ""
   ) {
-    next(errorHandler(400, 'All fields are required'));
+    next(errorHandler(400, "All fields are required"));
   }
 
-  const hashedPassword = bcryptjs.hashSync(password, 10)
-  const newUser = new User({ username, email, password:hashedPassword});
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+  const newUser = new User({ username, email, password: hashedPassword });
 
   try {
     await newUser.save();
@@ -28,6 +29,43 @@ const signUp = async (req, res, next) => {
   } catch (error) {
     next(error);
   }
+};
+
+// user sign in- 
+const signIn = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password || email === "" || password === "") {
+    next(errorHandler(400, "All fields are required"));
+  }
+
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(400, "User not found"));
+    }
+
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+     return next(errorHandler(400, "Invalid password")); 
+    }
+
+    const token = jwt.sign(
+      { userId: validUser._id },
+      process.env.JWT_SECRET_KEY,
+      { expiresIn: "1h" }
+    );
+
+    const {password: inputPassword, ...rest} = validUser._doc;
+    
+    res
+    .status(200)
+    .cookie('access_token', token, {httpOnly: true})
+    .json(rest)
+
+  } catch (error) {
+    next(error); 
+  }
 }; 
 
-export { signUp };
+export { signUp, signIn };
