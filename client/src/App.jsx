2c573a558b87179cf62859d354975787a2abaff2
SATYAM KUMAR PANDEY <defencepur360@gmail.com>
2024-07-24 18:23:31 +0530

    make the dashboard private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
-import {BrowserRouter as Router , Routes, Route} from "react-router-dom";
-import Home from "./pages/Home.jsx"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.jsx";
 import About from "./pages/About.jsx";
 import SignIn from "./pages/SignIn.jsx";
 import SignUp from "./pages/SignUp.jsx";
@@ -7,22 +7,24 @@ import Project from "./pages/Project.jsx";
 import Dashboard from "./pages/Dashboard.jsx";
 import Header from "./components/Header.jsx";
 import FooterComp from "./components/Footer.jsx";
+import PrivateRoute from "./components/PrivateRoute.jsx";
 
 function App() {
   return (
-   <Router>
-   <Header/>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/sign-in" element={<SignIn />} />
-      <Route path="/sign-up" element={<SignUp />} />
-      <Route path="/project" element={<Project />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-    </Routes>
-    <FooterComp/>
-   </Router>
-
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/project" element={<Project />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
+      </Routes>
+      <FooterComp />
+    </Router>
   );
 }
 
